055d0cb9dec4b71877537c958f31fb63ed7ce8d2
Ryan Romanchuk <rromanchuk@gmail.com>
2020-06-20 18:12:24 -0700

    add backward compatibility key

--- source/output-validate/index.js
+++ source/output-validate/index.js
@@ -119,6 +119,7 @@ exports.handler = async (event) => {
       let lastImg = thumbNails.Contents.pop();
       data.thumbNails.push(`s3://${data.destBucket}/${lastImg.Key}`);
       data.thumbNailsUrls.push(`https://${data.cloudFront}/${lastImg.Key}`);
+      data.thumbNailUrl.push(`https://${data.cloudFront}/${lastImg.Key}`);
     } else {
         throw new Error('MediaConvert Thumbnails not found in S3');
     }
